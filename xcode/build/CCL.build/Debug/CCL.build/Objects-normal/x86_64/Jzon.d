e5dda78c65f5d27f75304c03e343e44da3d93cd2
David Wicks <wicks.db@gmail.com>
2015-08-30 12:20:28 -0400

    Revert "Remove build products from repo."
    This reverts commit b4f2694ed39a5a4e82144e97ed29965e17cc053e.

---
+++ xcode/build/CCL.build/Debug/CCL.build/Objects-normal/x86_64/Jzon.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/Craig/Documents/cinder_glNext090/gl_Next090\ projects/CCL/src/Jzon.cpp \
+  /Users/Craig/Documents/cinder_glNext090/gl_Next090\ projects/CCL/src/Jzon.h
---
+++ xcode/build/CCL.build/Debug/CCL.build/Objects-normal/x86_64/Skeleton.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/Craig/Documents/cinder_glNext090/gl_Next090\ projects/CCL/src/Skeleton.cpp \
+  /Users/Craig/Documents/cinder_glNext090/gl_Next090\ projects/CCL/src/Skeleton.h
